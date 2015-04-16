239d560ce325be6212fd644bb032f82579e7c1af
jmoseman01 <jmoseman01@gmail.com>
2015-04-16 17:06:29 -0500

    reverted to just viewer instead of dashboard

---
+++ src/main/java/com/springMvcMongodb/SpringMVCmongoDBTest/viewer/ViewerController.java
@@ -0,0 +1,29 @@
+package com.springMvcMongodb.SpringMVCmongoDBTest.viewer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewerController {
+	@RequestMapping(value = "viewer/Id/{id}", method = RequestMethod.GET)
+	public String vidById(Model model, @PathVariable String id) {
+		model.addAttribute("vidId", id);
+		
+		return "video/viewer";
+	}
+	
+//	@RequestMapping(value = "/viewer/Id/", method = RequestMethod.GET)
+//	public String vidById(Model model, @PathVariable String id) {
+//		model.addAttribute("vidId", id);
+//		
+//		return "video/viewer/Id/";
+//	}
+	
+	@RequestMapping(value = "/viewer", method = RequestMethod.GET)
+	public String index(Model model) {
+		
+		return "video/viewer";
+	}
+}
