29a0a3f102cc7bfb51bc7f12c31a9cc1ae76f7b0
jc <robored61389@hotmail.com>
2015-04-18 13:20:08 -0500

    4/18/15 Pushing again because git didn't add adminScript.js to online repository.

---
+++ src/main/webapp/resources/js/adminScript.js
@@ -0,0 +1,132 @@
+$(function() {
+	//Manage Users variables
+	var userDB, users, userDialog, userForm,
+		emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+		firstName = $("#firstName"),
+		lastName = $("#lastName"),
+		email = $("#email"),
+		password = $("#password"),
+		classMod = $("#classMod"),
+		classes = $("classes"),
+		allFields = $([]).add(lastName).add(firstName).add(email).add(password).add(classMod).add(classes),
+		tips = $(".validateTips");
+	
+	function updateTips(t){
+		tips
+			.text(t)
+			.addclass("ui-state-highlight");
+		setTimeout(function(){
+			tips.removeClass("ui-state-highlight", 1500);
+		}, 500);
+	}
+	
+	function checkLength( o, n, min, max ) {
+		if ( o.val().length > max || o.val().length < min ) {
+		o.addClass( "ui-state-error" );
+		updateTips( "Length of " + n + " must be between " +
+		min + " and " + max + "." );
+		return false;
+		} else {
+		return true;
+		}
+	}
+	
+	function checkRegexp( o, regexp, n ) {
+		if ( !( regexp.test( o.val() ) ) ) {
+		o.addClass( "ui-state-error" );
+		updateTips( n );
+		return false;
+		} else {
+		return true;
+		}
+	}
+	
+	function addUser() {
+		var valid = true;
+		allFields.removeClass( "ui-state-error" );
+		valid = valid && checkLength( lastName, "Last Name", 3, 16 );
+		valid = valid && checkLength( firstName, "First Name", 3, 16 );
+		valid = valid && checkLength( email, "email", 6, 80 );
+		valid = valid && checkLength( password, "password", 5, 16 );
+		valid = valid && checkRegexp( lastName, /^[a-z]([0-9a-z_\s])+$/i, "Last name may consist of a-z, 0-9, underscores, spaces and must begin with a letter." );
+		valid = valid && checkRegexp( firstName, /^[a-z]([0-9a-z_\s])+$/i, "First name may consist of a-z, 0-9, underscores, spaces and must begin with a letter." );
+		valid = valid && checkRegexp( email, emailRegex, "eg. name@service.com" );
+		valid = valid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
+		valid 
+		if ( valid ) {
+			$( "#users tbody" ).append( "<tr>" +
+			"<td>" + lastName.val() + "</td>" +
+			"<td>" + firstName.val() + "</td>" +
+			"<td>" + email.val() + "</td>" +
+			"<td>" + password.val() + "</td>" +
+			"<td>" + classMod.val() + "</td>" +
+			"<td>" + classes.val() + "</td>" +
+			"</tr>" );
+			userDialog.dialog( "close" );
+		}
+		return valid;
+	}
+	
+	userDialog = $( "#create-user-form" ).dialog({
+		autoOpen: false,
+		height: 300,
+		width: 350,
+		modal: true,
+		buttons: {
+			"Create User": addUser,
+			Cancel: function() {
+				userDialog.dialog( "close" );
+			}
+		},
+		close: function() {
+			userForm[ 0 ].reset();
+			allFields.removeClass( "ui-state-error" );
+		}
+	});
+	
+	userForm = userDialog.find( "form" ).on( "submit", function( event ) {
+		event.preventDefault();
+		addUser();
+	});
+	
+	$( "#create-user" ).button().on( "click", function() {
+		userDialog.dialog( "open" );
+	});
+		
+	/*$( "#playlistsBox" ).dialog({
+		autoOpen: false,
+		show: {
+			effect: "fade",
+			duration: 700
+		},
+		hide: {
+			effect: "fade",
+			duration: 700
+		}
+	});*/
+	
+	$( "#playlistsButton" ).click(function(){
+		$( "#playlistsBox").dialog( "open" );
+	});
+	
+	$( "#edit-class" ).dialog({
+		autoOpen: false,
+		show: {
+			effect: "fade",
+			duration: 700
+		},
+		hide: {
+			effect: "fade",
+			duration: 700
+		}
+	});
+		
+	$( "#classesButton" ).click(function(){
+		$( "#classesBox").dialog( "open" );
+	});
+		
+	$( "#tabs" ).tabs({
+		collapsible: true,
+		active: false
+	});
+});
